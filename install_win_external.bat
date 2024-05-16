3e8104e72ccd17ae896f72a1702e19b31e185e87
FMDmitry <124060615+FMDmitry@users.noreply.github.com>
2024-05-16 20:57:44 +0300

    Delete install_win_external.bat

--- install_win_external.bat
+++
@@ -1,9 +0,0 @@
-
-git clone https://github.com/gabime/spdlog.git
-echo 'spdlog install complide...'
-git clone https://github.com/glfw/glfw.git
-echo 'glfm install complide...'
-git clone https://github.com/ocornut/imgui.git
-echo 'imgui install complide...'
-
-echo 'Installation all complide'
